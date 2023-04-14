3cd7a3712a5dd3428df0512fb7bc57afd383be59
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-04-13 23:47:36 -0300

    feat(graphql): add graphql mercurius driver in app module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,14 +3,15 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from "./app/app.module";
+import { AppModule } from './app/app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
